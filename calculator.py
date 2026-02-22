4100827422f5c27a00c4b7c7e7085c8861ad16d1
akash638805 <akashsingh44813@gmail.com>
2026-02-23 00:19:08 +0530

    new

---
+++ calculator.py
@@ -0,0 +1,16 @@
+n=18
+m=6
+ans1=n+m
+print("The addition of ",n,"and",m,"is",ans1)
+ans2=n-m
+print("The substraction of ",n,"and",m,"is",ans2)
+ans3=n*m
+print("The multiplication of ",n,"and",m,"is",ans3)
+ans4=n/m
+print("The division of ",n,"and",m,"is",ans4)
+ans5=n%m
+print("The modulus of ",n,"and",m,"is",ans5)
+ans6=n**m
+print("The exponentiation of ",n,"and",m,"is",ans6)
+ans7=n//m
+print("The floor division of ",n,"and",m,"is",ans7)
---
+++ programm1.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print("The sum of ",a,"and",b,"is",a+b)
